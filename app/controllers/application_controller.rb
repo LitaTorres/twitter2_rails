9073dc217b5478b148504952e7f6e9170d45336a
Leslie Torres <Leslietorresc@gmail.com>
2023-07-04 01:12:48 -0400

    Coomit aplicando estilos parte 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
     Pagy::DEFAULT[:items] = 10
     include Pagy::Backend
+
+    require 'pagy/extras/bootstrap'
 end
+
+#para agregar bootstrap en la paginación
